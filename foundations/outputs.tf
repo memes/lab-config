0163513535f471c9c568bb590070953449977f3d
Matthew Emes <memes@matthewemes.com>
2019-09-28 18:37:23 -0700

    Add TF SA description to output

--- foundations/outputs.tf
+++ foundations/outputs.tf
@@ -1,12 +1,13 @@
 output "project_id" {
   description = <<EOD
-The project id for lab foundations project
+The project id for lab foundations project.
 EOD
   value       = google_project.lab-config.project_id
 }
 
 output "tf_sa" {
   description = <<EOD
+The service account used by Terraform.
 EOD
   value       = google_service_account.terraform.email
 }
