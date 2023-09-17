eff06b8c83efd5643aab3419655fe8acca79fc69
Matthew Emes <memes@matthewemes.com>
2023-09-17 16:50:02 -0700

    fix: Regenerate intermediate CA cert

--- vault/pki.tf
+++ vault/pki.tf
@@ -4,7 +4,7 @@
 
 locals {
   # Change when it is time to rotate intermediate cert
-  intermediate_cn = "Accelerated GCP Lab Intermediate CA April 2022"
+  intermediate_cn = "Accelerated GCP Lab Intermediate CA 2023"
 }
 
 resource "vault_pki_secret_backend_config_urls" "pki_ca" {
@@ -30,7 +30,7 @@ resource "vault_pki_secret_backend_intermediate_cert_request" "intermediate" {
   exclude_cn_from_sans = true
   ou                   = "Lab CA"
   organization         = "Accelerated GCP"
-  country              = "United States"
+  country              = "US"
   locality             = "Laguna Niguel"
   province             = "California"
 }
@@ -86,7 +86,7 @@ resource "vault_pki_secret_backend_role" "server" {
   ]
   ou           = ["Lab"]
   organization = ["Accelerated GCP"]
-  country      = ["United States"]
+  country      = ["US"]
   locality     = ["Laguna Niguel"]
   province     = ["California"]
   require_cn   = true
@@ -118,7 +118,7 @@ resource "vault_pki_secret_backend_role" "person" {
   ]
   ou           = ["Lab"]
   organization = ["Accelerated GCP"]
-  country      = ["United States"]
+  country      = ["US"]
   locality     = ["Laguna Niguel"]
   province     = ["California"]
   require_cn   = true
@@ -150,7 +150,7 @@ resource "vault_pki_secret_backend_role" "server_2048" {
   ]
   ou           = ["Lab"]
   organization = ["Accelerated GCP"]
-  country      = ["United States"]
+  country      = ["US"]
   locality     = ["Laguna Niguel"]
   province     = ["California"]
   require_cn   = true
